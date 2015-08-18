2b430ad319ecf80ec5e886860f017ef455422cd2
JGladysh <jgladysh@gmail.com>
2015-08-17 17:39:51 -0700

    - Testing of multiple editors creation

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var Editus = require('editus');
 var keyWordsArray = ["create", "experiment", "assign", "to", "all", "users", "where", "for", "salt", "new"];
 
 window.onload = function(){
-    Editus.createEditor('content');
-    Editus.setHighlightingWords(keyWordsArray);
+    var editor1 = Editus.initEditus('content1');
+    editor1.setHighlightingWords(keyWordsArray);
+    var editor2 = Editus.initEditus('content2');
+    editor2.setHighlightingWords(['a','b','c']);
 };
